4e1ba3aea961fd10d763be28e23fcc7faecbf2d3
Himanshu Kumar Singh <justjustifying@outlook.com>
2023-05-13 23:44:00 +0530

    Project Complete

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ window.onload=()=>{
 const buttons=document.querySelectorAll(".numbers");
 const screen=document.querySelector(".screen");
 const equals=document.querySelector("#btn-equals")
+const clear=document.querySelector("#btn-clear");
+
 
 buttons.forEach(button=>{
 button.addEventListener('click',(e)=>{
@@ -16,7 +18,21 @@ screen.value+=e.target.innerHTML;
 
 equals.addEventListener('click',(e)=>{
 
+    if(!screen.value){
+        screen.value="Enter Value"
+    }else{
+        screen.value=eval(screen.value);
+
+
+    }
+
 })
 
 })
+
+
+
+    clear.addEventListener('click',()=>{
+        screen.value="";
+    })
 }
