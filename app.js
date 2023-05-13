f05d660f55fa9f44b92a17a76c8f4ceca0c467da
Himanshu Kumar Singh <justjustifying@outlook.com>
2023-05-13 23:37:28 +0530

    Adding Equals

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 window.onload=()=>{
 const buttons=document.querySelectorAll(".numbers");
 const screen=document.querySelector(".screen");
-
+const equals=document.querySelector("#btn-equals")
 
 buttons.forEach(button=>{
 button.addEventListener('click',(e)=>{
 
+
+    //console.log(e.target.innerHTML);
+
+screen.value+=e.target.innerHTML;
+
+
 })
 
+equals.addEventListener('click',(e)=>{
 
+})
 
 })
 }
